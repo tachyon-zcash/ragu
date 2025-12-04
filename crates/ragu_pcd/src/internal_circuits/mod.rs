e8941e469659f703f985f1761fdf7f528264568a
Sean Bowe <ewillbefull@gmail.com>
2025-12-04 05:47:36 -0700

    pcd: simplify staged polynomial addition to mesh

--- crates/ragu_pcd/src/internal_circuits/mod.rs
+++ crates/ragu_pcd/src/internal_circuits/mod.rs
@@ -1,9 +1,5 @@
 use arithmetic::Cycle;
-use ragu_circuits::{
-    mesh::MeshBuilder,
-    polynomials::Rank,
-    staging::{StageExt, StagedCircuit},
-};
+use ragu_circuits::{mesh::MeshBuilder, polynomials::Rank, staging::StageExt};
 use ragu_core::Result;
 
 pub mod c;
@@ -12,9 +8,9 @@ pub mod stages;
 pub mod unified;
 
 const DUMMY_CIRCUIT_ID: usize = 0;
-const C_CIRCUIT_ID: usize = 1;
-const NATIVE_PREAMBLE_CIRCUIT_ID: usize = 2;
-const C_STAGED_ID: usize = 3;
+const C_STAGED_ID: usize = 1;
+const C_CIRCUIT_ID: usize = 2;
+const NATIVE_PREAMBLE_CIRCUIT_ID: usize = 3;
 
 pub fn index(num_application_steps: usize, internal_index: usize) -> usize {
     num_application_steps + super::step::NUM_INTERNAL_STEPS + internal_index
@@ -25,12 +21,13 @@ pub fn register_all<'params, C: Cycle, R: Rank>(
     params: &'params C,
 ) -> Result<MeshBuilder<'params, C::CircuitField, R>> {
     let mesh = mesh.register_circuit(dummy::Circuit)?;
-    let mesh = mesh.register_circuit(c::Circuit::<C, R>::new(params.circuit_poseidon()))?;
+    let mesh = {
+        let c = c::Circuit::<C, R>::new(params.circuit_poseidon());
+        mesh.register_circuit_object(c.final_into_object()?)?
+            .register_circuit(c)?
+    };
+
     let mesh =
         mesh.register_circuit_object(stages::native::preamble::Stage::<C, R>::into_object()?)?;
-    let mesh = mesh
-        .register_circuit_object(
-            <c::Circuit<C, R> as StagedCircuit<C::CircuitField, R>>::Final::final_into_object()?,
-        )?;
     Ok(mesh)
 }
